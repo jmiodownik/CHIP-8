f023bbd486f619b08bdb5688c0c9b5340825c6e1
Snoogie <>
2017-06-07 15:33:37 +0000

    added a bunch more error checking, and wrote the file input handling

--- include/system.h
+++ include/system.h
@@ -6,7 +6,8 @@ typedef struct chip8_t chip8;
 chip8* create_chip8(void);
 int c8_initialize(chip8*);
 void c8_emulate_cycle(chip8*);
-int c8_load_game(chip8*);
+int c8_load_program(chip8*, const char*);
+void c8_shutdown(chip8*);
 
 
 //Chip-8 getters
--- src/main.c
+++ src/main.c
@@ -14,39 +14,54 @@
 int main(int argc, char** argv)
 {
 
+	if(argc != 2)
+	{
+		errno = 5;
+		log_err("Input file needs to be specified as command line argument");
+		return 1;
+	}
+	
+	
+
 	//this needs to be checked better
 	chip8* chip8 = create_chip8();
-	
+
 	int ret = c8_initialize(chip8);
-	check(ret != 1, "Initialization failed");
-	
-//	setup_graphics();
-//	setup_input();
+	if(ret) return 1;
 
-//	initialize();
-//	load_game();
-//
-
-
-
-//Test the stack
+	//Test the stack
 #ifdef DEBUG
 	test_stack(chip8);
 #endif
 
-/*
-	for(;;)
-	{
-		c8_emulate_cycle(chip8);
+	char* fName = argv[1];
 
-		if(c8_get_draw_flag(chip8)){
-			draw_graphics();
-		}
+	ret = c8_load_program(chip8, fName);
+	if(ret) return 1;
 
-		set_keys(chip8);
-	}
-*/
+
+	c8_shutdown(chip8);
+	//	setup_graphics();
+	//	setup_input();
+
+	//	initialize();
+	//	load_game();
+	//
+
+
+
+
+	/*
+	   for(;;)
+	   {
+	   c8_emulate_cycle(chip8);
+
+	   if(c8_get_draw_flag(chip8)){
+	   draw_graphics();
+	   }
+
+	   set_keys(chip8);
+	   }
+	   */
 	return 0;
-error:
-	return 1;
 }
